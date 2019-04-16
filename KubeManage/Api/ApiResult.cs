ba8d9160f6609c035275bbda1ff22d6109dd9fba
wangjun <wangjun.aspx@qq.com>
2019-04-16 22:25:43 +0800

    Update ApiResult.cs

--- KubeManage/Api/ApiResult.cs
+++ KubeManage/Api/ApiResult.cs
@@ -4,11 +4,11 @@ namespace KubeManage.Api
 {
     public class ApiResult<T> where T : class, new()
     {
-        [JsonProperty("code")] public int Code { get; set; } = 200;
+        public int Code { get; set; } = 200;
 
-        [JsonProperty("msg")] public string Message { get; set; } = "";
+        public string Message { get; set; } = "";
 
-        [JsonProperty("data")] public T Data { get; set; }
+        public T Data { get; set; }
     }
 
     public class ApiResult : ApiResult<object>
